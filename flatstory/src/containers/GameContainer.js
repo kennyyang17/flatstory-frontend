5ad9c224a17c96a6ee74e8efadba32a9991c552e
Kenny Yang <kennyyang19@gmail.com>
2018-09-27 19:22:31 -0500

    building phaser stuff

---
+++ flatstory/src/containers/GameContainer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Game from '../components/game/Game'
+
+ class GameContainer extends React.Component {
+
+   state = {
+      level: 'start',
+      score: null
+    }
+   
+   gameOver = (score) => {
+    this.setState({
+        level:'gameOver', 
+        score:score})
+  }
+
+   render(){
+    switch (this.state.level) {
+      case 'start':
+        return (
+          <div className="gameContainer">
+            <Game gameOver={this.gameOver}/>
+          </div>
+        )
+        break
+      case 'gameOver':
+        return (
+          <div className="gameContainer">
+            <h1>Score: {this.state.score}</h1>
+          </div>
+        )
+        break
+      default:
+        break
+    }
+  }
+}
+ export default GameContainer
